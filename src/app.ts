f0460a6bd9e3317faf902b4d0653aa985b5fec27
Abel Acuna <abel3121@gmail.com>
2024-02-22 14:55:46 -0500

    first commmit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
-import { createBot, MemoryDB, createProvider } from '@bot-whatsapp/bot'
+import { createBot, MemoryDB, createProvider  } from '@bot-whatsapp/bot'
+import MongoAdapter from '@bot-whatsapp/database/mongo';
 import { TelegramProvider } from '@builderbot-plugins/telegram'
 import { BaileysProvider } from '@bot-whatsapp/provider-baileys'
 
@@ -13,8 +14,13 @@ const main = async () => {
     const provider = createProvider(BaileysProvider)
     // const provider = createProvider(TelegramProvider, { token: process.env.TELEGRAM_API ?? '' })
 
+    const adapterDB = new MongoAdapter({
+        dbUri:
+          'mongodb+srv://demo:dWZjTx1q4Dl1Fbf8@cluster0.oodr2ap.mongodb.net/',
+        dbName: 'peluqueria',
+      });
     await createBot({
-        database: new MemoryDB(),
+        database: adapterDB,
         provider,
         flow: flows
     }, { extensions: { ai } })
